daed69352ff19a7522741d1382f7dd9195f5db0d
diego-ramos130 <diego.onethreezero@gmail.com>
2018-07-16 14:24:06 -0700

    populateCookiesPurchased is working

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
+'use strict';
 //what we want to do: make an object that represents the store with min, max customers and avg cookies bought at hourly standards. 
 var openHours = 15;
+var ulEl = document.createElement('ul');
 
 function add(el, el2) {
  return el + el2;
 }
 
 
-var firstandpike ={
-  minCustomers = 23,
-  maxCustomers = 65,
-  avgPerCustomer = 6.3,
-  cookiesPerHour = [],
+var firstandpike = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgPerCustomer: 6.3,
+  cookiesPerHour: [],
   customerGen: function() {
     var min = Math.ceil(this.minCustomers);
     var max = Math.floor(this.maxCustomers);
@@ -18,11 +20,22 @@ var firstandpike ={
   },
   populateCookiesPurchased: function (){
     for (var i = 0; i < openHours - 1; i++){
-      var cookiesPurchased = this.customerGen() * this.avgPerCustomer;
+      var cookiesPurchased = Math.floor(this.customerGen() * this.avgPerCustomer);
       this.cookiesPerHour.push(cookiesPurchased);
     }
   },
   calculateTotalCookies: function(cph){ 
     return cph.reduce(add, 0)
+  },
+  appendCookiesToHtml: function () {
+    for (var i = 0; i < this.cookiesPerHour.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = this.cookiesPerHour[i];
+      ulEl.appendChild(liEl);
+    }
+  },
+  applyUl: function () {
+    var cookiesReport = document.getElementById('sales');
+    cookiesReport.appendChild(ulEl);
   }
 }
